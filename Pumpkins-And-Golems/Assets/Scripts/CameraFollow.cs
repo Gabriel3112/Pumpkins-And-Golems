4ed70a030983c222931e5e9f12daa198b272b24a
Luiz Gabriel <47277838+Gabriel3112@users.noreply.github.com>
2020-07-01 18:29:52 -0300

    Finished

---
+++ Pumpkins-And-Golems/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform pumpkimPos;
+
+    private float zDistance = 25f;
+    private float yDistance = 4f;
+
+    private void Awake()
+    {
+        pumpkimPos = GameObject.Find("Pumpkim").transform;
+    }
+   
+    void Update()
+    {
+        Vector3 temp = transform.position;
+        temp.y = pumpkimPos.position.y + yDistance;
+        temp.z = pumpkimPos.position.z - zDistance;
+        transform.position = temp;
+    }
+}
---
+++ Pumpkins-And-Golems/Assets/Scripts/Pumpkim.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pumpkim : MonoBehaviour
+{
+    public float moveForce = 10.0f;
+    private Rigidbody rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y <= -40)
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        MovePumpkim();
+    }
+
+    void MovePumpkim()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        rb.AddForce(new Vector3(h * moveForce,0f,v * moveForce));
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Golem" || other.tag == "Gate")
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
